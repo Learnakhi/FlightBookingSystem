d19d22179a4e31f0e97d20393f62cd6b7c1ac3a3
Learnakhi <115710858+Learnakhi@users.noreply.github.com>
2022-12-14 21:53:08 +0530

    Ckeckin-microservice

---
+++ CheckinService (2) (1)/CheckinService/src/test/java/com/flightbooking/CheckinService/CheckinServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.flightbooking.CheckinService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
